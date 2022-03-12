192c6a459e9ded03cae6623a741a621cc91cc6e4
Simon Onen <sonen8524@gmail.com>
2022-03-11 18:43:11 -0800

    Reduce th number of putchar in code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,11 +18,7 @@ int main(void)
 			{
 				putchar(i);
 				putchar(j);
-				if ((i == '8') && (j == '9'))
-				{
-					putchar(' ');
-				}
-				else
+				if (!((i == '8') && (j == '9')))
 				{
 					putchar(',');
 					putchar(' ');
