f14028fa8ce25bc81220823ca28b8f9a3211b962
Simon Onen <sonen8524@gmail.com>
2022-03-11 18:39:51 -0800

    Add C Program to print combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '1';
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (i != j && (i != '9' && j != '0'))
+			{
+				putchar(i);
+				putchar(j);
+				if ((i == '8') && (j == '9'))
+				{
+					putchar(' ');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		j = '1';
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -29,3 +29,6 @@
 
 ## 9. Patience, persistence and perspiration make an unbeatable combination for success
 - A program that prints all possible combinations of single-digit numbers.
+
+## 10. Inventing is a combination of brains and materials. The more brains you use, the less material you need
+- A program that prints all possible different combinations of two digits.
