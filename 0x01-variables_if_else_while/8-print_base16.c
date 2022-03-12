40d5e3e8cdec20183acd91cc9d95d561bfb7b1a8
Simon Onen <sonen8524@gmail.com>
2022-03-11 16:07:26 -0800

    Add C programto print lower case hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int i = '0';
+	int j = 'a';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j <= 'f')
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -23,3 +23,6 @@
 
 ## 7. Smile in the mirror
 - A program that prints the lowercase alphabet in reverse, followed by a new line.
+
+## 8. Hexadecimal
+- A program that prints all the numbers of base 16 in lowercase, followed by a new line.
