33dd49bd4874278640a2ef44e770bbc474586935
Simon Onen <sonen8524@gmail.com>
2022-03-15 16:24:09 -0700

    Add correct data type for _islower function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,20 @@
 /**
  * _islower - checks for lowercase characters
  *
+ * @c: parameter of type int
+ *
  * Return: If the islower(c) is 1 the function returns 1
  * else if the islower(c) is 0 the function returns 0
  */
-int _islower(void)
+int _islower(int c)
 {
-if (islower(c))
-{
-return (1);
-}
-else
-{
-return (0);
-}
-return (0);
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
 }
