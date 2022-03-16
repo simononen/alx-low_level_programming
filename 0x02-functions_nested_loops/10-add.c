f5dbd7636702234ad62cdff12e00f4f7e7e046d5
Simon Onen <sonen8524@gmail.com>
2022-03-15 19:34:13 -0700

    Add C program to add two integer numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,28 +1,12 @@
 #include "main.h"
 /**
- * jack_bauer - Print every minute of the day of Jack Bauer
+ * add- adds two numbers
+ * @p: int parameter
+ * @q: int parameter
  *
  * Return: Returns Nothing (void)
  */
-void jack_bauer(void)
+int add(int p, int q)
 {
-	int p, q;
-
-	p = 0;
-	q = 0;
-	while (p < 24)
-	{
-		while (q < 60)
-		{
-			_putchar(p / 10 + 48);
-			_putchar(p % 10 + 48);
-			_putchar(':');
-			_putchar(q / 10 + 48);
-			_putchar(q % 10 + 48);
-			_putchar('\n');
-			q++;
-		}
-	       p++;
-	       q = 0;
-	}
+	return (p + q);
 }
