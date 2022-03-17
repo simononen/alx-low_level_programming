14c64f479aa72cf2f94443f75d1380ff71cee77d
Simon Onen <sonen8524@gmail.com>
2022-03-16 17:32:35 -0700

    Add modifications to the 11-print_to_98 C program

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,86 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - Print numbers up to 98
  * @n: int parameter
  *
  * Return: Returns Nothing (void)
  */
+
 void print_to_98(int n)
 {
-	int final_number = 98;
+	int k;
+	int MAX_NUMBER = 98
 
-	if (n < final_number)
+	if (n <= MAX_NUMBER)
 	{
-		for (; n <= final_number; n++)
+		k = n;
+		while (k < MAX_NUMBER)
 		{
-			if (n == 98)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				break;
-			}
-			else
-			{
-				if (n >= -9 && n <= 9)
-				{
-					if (n < 0)
-					{
-						_putchar('-');
-						_putchar((n * -1) + '0');
-						_putchar(',');
-						_putchar(' ');
-					}
-					else if (n >= 0)
-					{
-						_putchar(n + '0');
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				else if (n >= 100)
-				{
-					_putchar((n / 100) + '0');
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
+			printf("%d", k);
+			k++;
 		}
-		_putchar('\n');
-	}
-	else if (n == 0)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		_putchar('\n');
 	}
 	else
 	{
-		for (; n >= final_number; n--)
+		k = n;
+		while (k > MAX_NUMBER)
 		{
-			if (n == 98)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				break;
-			}
-			else
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d", k);
+			k--;
 		}
-		_putchar('\n');
 	}
-	return;
+	printf("98\n");
 }
+
