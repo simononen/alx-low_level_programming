adf7412662fc8d39a3a7bae1b38dada62367b5f8
Simon Onen <sonen8524@gmail.com>
2022-03-17 15:57:50 -0700

    Add C program to print numbers 0 to 9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers from 0 to 9
+ *
+ * Return: Nothing (void)
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		if (!(i == '2' || i == '4'))
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
