99a3c242bf0076969871c535e85c9dc0a41ea52f
Simon Onen <sonen8524@gmail.com>
2022-03-16 18:28:58 -0700

    Add C program to print the sum of even fibonacci numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,22 +7,22 @@
  */
 int sum_even_fibonacci(void)
 {
-	int j, MAX_FIBONACCI, sum;
+	int MAX_FIBONACCI, sum;
 	long fib1, fib2, fibn;
 
-	j = 0;
-	fib1 = 1,
-	fib2 = 2;
+	fib1 = 0,
+	fib2 = 1;
+	fibn = 0;
 	MAX_FIBONACCI = 4000000;
 	sum = 0;
 
-	while (j < MAX_FIBONACCI)
+	while (fibn < MAX_FIBONACCI)
 	{
 		fibn =  fib1 + fib2;
 		fib1 = fib2;
 		fib2 = fibn;
-		(fibn % 2 == 0) ? sum += fibn : ' ';
-		j++;
+		if (fibn % 2 == 0)
+			sum += fibn;
 	}
 	printf("%d\n", sum);
 	return (0);
