3f4b31a22ca8e90d44ef91c54d71b9fef78eee8e
Simon Onen <sonen8524@gmail.com>
2022-03-17 15:19:25 -0700

    Add C program to multiply two int numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Multiply two int numbers
+ *
+ * @a: int paramter first
+ * @b: int paramter second
+ *
+ * Return: Always(a * b) Success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
