dcfc2fcd57613a8c3d1609a3d46401adab15008f
Simon Onen <sonen8524@gmail.com>
2022-03-16 19:34:16 -0700

    Add C program to print fibonacci starting with 1 and 2

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * print_fibonacci_98 - Print fibonacci numbers from 1 up to 98
+ *
+ * Return: Nothing (void)
+ */
+void print_fibonacci_98(void)
+{
+	int fibo_1, fibo_2, fibo_n;
+	int j, MAX_NUMBER;
+
+	MAX_NUMBER = 98;
+	fibo_1 = 1;
+	fibo_2 = 2;
+	j = 1;
+	while (j <= MAX_NUMBER)
+	{
+		if (j == MAX_NUMBER)
+		{
+			printf("%d", fibo_2);
+		}
+		else
+		{
+			printf("%d, ", fibo_1);
+			fibo_n = fibo_1 + fibo_2;
+			fibo_1 = fibo_2;
+			fibo_2 = fibo_n;
+		}
+		j++;
+	}
+	printf("\n");
+}
+
+/**
+ * main - prints fibonnaci numbers
+ *
+ * Description: prints first 98 fibonacci
+ * Return: Always(0) Success
+ */
+
+int main(void)
+{
+
+	print_fibonacci_98();
+	return (0);
+}
