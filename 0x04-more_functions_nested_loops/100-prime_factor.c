b59443fdf29de9224acb2f28ce2b988f8d5591ab
Simon Onen <sonen8524@gmail.com>
2022-03-17 18:08:22 -0700

    Add C program to print largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -13,21 +13,21 @@ void print_largest_prime_factor(void)
 
 	while (x % 2 == 0)
 	{
-		pf = 2;
+		prime_factor = 2;
 		x = x / 2;
 	}
 	for (i = 3; i <= x / 2; i = i + 2)
 	{
 		while (x % i == 0)
 		{
-			pf = i;
+			prime_factor = i;
 			x = x / i;
 		}
 	}
 	if (x > 2)
-		pf = x;
+		prime_factor = x;
 
-	printf("%ld\n", pf);
+	printf("%ld\n", prime_factor);
 }
 
 /**
