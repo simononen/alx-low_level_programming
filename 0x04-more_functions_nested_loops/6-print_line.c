c21a722714a67912e11e2446ebb0910019f922ba
Simon Onen <sonen8524@gmail.com>
2022-03-17 17:27:57 -0700

    Add C program to print line based on the number of characters

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_line - prints line with n characters
+ *
+ * @n: int parameter for number of characters
+ *
+ * Return: Nothing (void)
+ */
+
+void print_line(int n)
+{
+	int i;
+	int num = n
+
+	if (num <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		i = 1;
+		while (i <= num)
+		{
+			_putchar('_';)
+			i++;
+		}
+		_putchar('\n');
+	}
+}
