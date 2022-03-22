74c9182a0dde6cd85f155eca3eee8ccd93f20731
Simon Onen <sonen8524@gmail.com>
2022-03-21 18:56:32 -0700

    Add C program file 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Reset value in Address referenced by pointer
+ *
+ * @n: pointer parameter
+ *
+ * Return: Nothing (void)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
