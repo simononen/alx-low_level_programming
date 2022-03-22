0379e3136614d590024fce825962430e752972ba
Simon Onen <sonen8524@gmail.com>
2022-03-22 00:57:06 -0700

    Ass C program function to print n elements of array integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ *
+ * @a: pointer parameter to array
+ *
+ * @n: parameter for number of array elements
+ *
+ * Return: Nothing (void)
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
