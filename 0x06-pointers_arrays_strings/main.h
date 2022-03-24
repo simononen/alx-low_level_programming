fadfecbd8b442c4c93ce64271ec54de74bd53fdb
Simon Onen <sonen8524@gmail.com>
2022-03-23 23:30:42 -0700

    Add C programfunction to add two numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *rot13(char *);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
